648f9ee354f4a548ba766e8570c749923ee7e4c2
ADhdCZ <mradamuher@seznam.cz>
2024-08-16 00:07:33 +0200

    Matrix sekvence přidána Změna a oprava config.ini

--- program/main.py
+++ program/main.py
@@ -1,32 +1,68 @@
 import pyautogui
 import time
 import math
+import itertools
+import random
+import os
+import threading
 
 pyautogui.FAILSAFE = False
 
 def move_mouse_in_circle(radius, center_x, center_y, steps=66):
     for i in range(steps):
-        # Výpočet úhlu (ve stupních) a převod na radiány
         angle = (2 * math.pi * i) / steps
         x = center_x + int(radius * math.cos(angle))
         y = center_y + int(radius * math.sin(angle))
-        
-        # Pohyb myši na vypočítané souřadnice
         pyautogui.moveTo(x, y, duration=0.01)
 
 def move_mouse_and_press_key():
     screen_width, screen_height = pyautogui.size()
     center_x = screen_width // 2
     center_y = screen_height // 2
-    radius = min(center_x, center_y) // 3  # Poloměr kružnice
+    radius = min(center_x, center_y) // 3
 
     while True:
         move_mouse_in_circle(radius, center_x, center_y)
-        
-        # Simulace stisknutí klávesy
-        pyautogui.press('shift')  # Stiskne klávesu Shift
-        
-        time.sleep(5)  # Pauza mezi akcemi
+        pyautogui.press('shift')
+        time.sleep(5)
+
+messages = [
+    "System initializing...",
+    "Loading modules...",
+    "Connecting to mainframe...",
+    "Decrypting data...",
+    "Transmission received.",
+    "Hacking the mainframe...",
+    "User authenticated.",
+    "Rebooting system...",
+    "Encrypting data...",
+    "Posun running..."
+]
+
+message_cycle = itertools.cycle(messages)
+
+def generate_matrix_line():
+    length = random.randint(10, 100)
+    line = ''.join(random.choice('01') for _ in range(length))
+    return line
+
+def display_matrix():
+    try:
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            for _ in range(20):
+                print(generate_matrix_line())
+            print("\n")
+            print(next(message_cycle))
+            time.sleep(1.0)
+    except KeyboardInterrupt:
+        print("Script was stopped by the user.")
 
 if __name__ == "__main__":
-    move_mouse_and_press_key()
+    # Spustí pohyb myši a stisk klávesy ve zvláštním vlákně
+    mouse_thread = threading.Thread(target=move_mouse_and_press_key)
+    mouse_thread.daemon = True  # Zajištění, že se vlákno ukončí s hlavním programem
+    mouse_thread.start()
+
+    # Spustí zobrazení Matrixu v hlavním vlákně
+    display_matrix()
